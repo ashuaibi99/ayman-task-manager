f952f9a33c734b01a11c09675130f1a563e9a514
Ayman <ashuaibi3@hawk.iit.edu>
2024-10-16 16:42:10 -0500

    vite/shadcn/tailwind/typescript set up

---
+++ task-manager/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="bg-zinc-950 dark:bg-white" >
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
