fad807b0b5ae9fe7351282d170ac48acd61bd66e
Ayman <ashuaibi3@hawk.iit.edu>
2024-10-17 16:48:09 -0500

    Added base table, fixed input and add task fields

--- task-manager/src/App.tsx
+++ task-manager/src/App.tsx
@@ -1,11 +1,98 @@
 import './App.css'
+import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/ui/theme-provider"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+
+const invoices = [
+  {
+    invoice: "INV001",
+    paymentStatus: "Paid",
+    totalAmount: "$250.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV002",
+    paymentStatus: "Pending",
+    totalAmount: "$150.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV003",
+    paymentStatus: "Unpaid",
+    totalAmount: "$350.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV004",
+    paymentStatus: "Paid",
+    totalAmount: "$450.00",
+    paymentMethod: "Credit Card",
+  },
+  {
+    invoice: "INV005",
+    paymentStatus: "Paid",
+    totalAmount: "$550.00",
+    paymentMethod: "PayPal",
+  },
+  {
+    invoice: "INV006",
+    paymentStatus: "Pending",
+    totalAmount: "$200.00",
+    paymentMethod: "Bank Transfer",
+  },
+  {
+    invoice: "INV007",
+    paymentStatus: "Unpaid",
+    totalAmount: "$300.00",
+    paymentMethod: "Credit Card",
+  },
+]
 
 function App() {
   return (
-    <div className="bg-zinc-950 dark:bg-white" >
-      <Button>Click me</Button>
-    </div>
+    <ThemeProvider defaultTheme="dark">
+      <div className="flex h-screen mt-32">
+        <div className="bg-zinc dark w-80"  >
+          <Input type="text" placeholder="Enter Your Task!" />
+        </div>
+        <div className="bg-zinc dark flex ml-4 " >
+          <Button variant="outline"> Add Task </Button> 
+        </div>
+        <div>
+          <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Task</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {invoices.map((invoice) => (
+            <TableRow key={invoice.invoice}>
+              <TableCell className="font-medium">{invoice.invoice}</TableCell>
+              <TableCell>{invoice.paymentStatus}</TableCell>
+              <TableCell>{invoice.paymentMethod}</TableCell>
+              <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
