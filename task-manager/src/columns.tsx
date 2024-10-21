644391ca782f2a2bbc78d7494eb73564d92911b9
Ayman <ashuaibi3@hawk.iit.edu>
2024-10-21 15:19:25 -0500

    add actions tab containing delete

--- task-manager/src/columns.tsx
+++ task-manager/src/columns.tsx
@@ -2,6 +2,15 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import { Checkbox } from "@/components/ui/checkbox"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button"
 
 export type Task = {
   id: number
@@ -30,4 +39,30 @@ export const columns: ColumnDef<Task>[] = [
     accessorKey: "dateCreated",
     header: "Date Created",
   },
+  {
+    accessorKey: "actions",
+    header: "Actions",
+    cell: ({ row }) => {
+ 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => console.log("yes")}
+            >
+              Delete
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
+
 ]
